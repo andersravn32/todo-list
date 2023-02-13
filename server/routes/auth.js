53d2c703f11c772dc1c151f242b8d722dfed4eca
Anders Ravn <anders@adriansen.dk>
2023-02-13 12:26:07 +0100

    Add: Added auth router

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,4 +1,16 @@
 const express = require("express");
 const router = express.Router();
 
+// POST: /auth/signin, handles signin requests, return accessToken, refreshToken and user data
+router.post("/signin", require("../controllers/signin"));
+
+// POST: /auth/signup, handles up requests, returns accessToken and user data if signup was successful
+router.post("/signup", require("../controllers/signup"));
+
+// POST: /auth/signup, handles up requests, returns accessToken and user data if signup was successful
+router.post("/signout", require("../controllers/signout"));
+
+// POST: /auth/refresh, handles refresh requests, returns new accessToken and fresh user data
+router.post("/refresh", require("../controllers/refresh"));
+
 module.exports = router;
