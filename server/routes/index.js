4e6e89e68e15ba20033e59cf19877b11617e2abf
Anders Ravn <anders@adriansen.dk>
2023-02-13 12:55:37 +0100

    Add: Added route protection to tasks routes

--- server/routes/index.js
+++ server/routes/index.js
@@ -2,7 +2,8 @@ const express = require("express");
 const router = express.Router();
 const check = require("../middleware/check");
 
-
+// Tasks routes
+router.use("/tasks", check.auth, require("./tasks"));
 
 // Auth routes
 router.use("/auth", require("./auth"));
