27a724f71d7e0d3533c1712549835d8edce8f47d
Anders Ravn <anders@adriansen.dk>
2023-02-13 11:12:56 +0100

    Add: Added primary server setup

---
+++ server/server.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const http = require("http");
+const app = express();
+const server = http.createServer(app);
+const dotenv = require("dotenv");
+const cors = require("cors");
+
+// Async init method, for creating initial database connection
+const init = async () => {
+
+    // Configure environment varibles
+    dotenv.config();
+
+    // Create database connection pool
+    console.log("Connected to database")
+
+    // Configure cors to accept every connection
+    app.use(cors({
+        origin: "*"
+    }))
+
+    // Route traffic to router
+    app.use(require("./routes"));
+
+    server.listen(process.env.PORT, () => {
+        console.log("Server is ready for requests")
+    });
+}
