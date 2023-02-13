c7a6098e35132db071dd585477a1e6e9f67f7df7
Anders Ravn <anders@adriansen.dk>
2023-02-13 12:55:18 +0100

    Add: Added primary jwt check middleware

--- server/middleware/check.js
+++ server/middleware/check.js
@@ -1,5 +1,33 @@
+const jwt = require("jsonwebtoken");
+
 module.exports = {
-    auth: async (req, res, next) => {
-        
+  auth: async (req, res, next) => {
+    // Get token from request headers or request query
+    const token = req.headers.authorization || req.query.token;
+
+    if (!token) {
+      return res.json({
+        message: "Missing access token",
+      });
     }
-}
+
+    try {
+      // Extract user data from jwt
+      const tokenData = jwt.verify(token, process.env.JWT_AUTH);
+      delete tokenData.iat;
+      delete tokenData.exp;
+      delete tokenData.type;
+
+      // Assign req.user to data extracted from token
+      req.user = tokenData;
+
+      // Complete middleware
+      next();
+    } catch (error) {
+      return res.json({
+        message: "Provided access token has either expired or is invalid",
+        ...error,
+      });
+    }
+  },
+};
