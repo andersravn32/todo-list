c1456260892ff77a424c8895033e3ff4ae7ea5d7
Anders Ravn <anders@adriansen.dk>
2023-02-13 12:25:58 +0100

    Add: Added signout function

--- server/controllers/signout.js
+++ server/controllers/signout.js
@@ -1,3 +1,33 @@
+const database = require("../utilities/database");
+
 module.exports = async (req, res) => {
-    
-}
+  // Get jwt from request body
+  if (!req.body.token) {
+    return res.json({
+      message: "Missing jwt",
+    });
+  }
+
+  try {
+    // Get database connection
+    const db = database.get();
+
+    // Delete user providede refresh token
+    const tokenDelete = await db
+      .collection("tokens")
+      .deleteOne({ token: req.body.token, type: "refresh" });
+
+    // If no token was deleted from database, return error to user
+    if (!tokenDelete.acknowledged || !tokenDelete.deletedCount) {
+      return res.json({
+        message: "Failed to signout",
+      });
+    }
+
+    return res.json({
+        message: "Signed out"
+    })
+  } catch (error) {
+    return res.json(error);
+  }
+};
