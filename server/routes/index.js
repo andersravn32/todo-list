02fc20c51601333aaadfa20611d246f943ae927e
Anders Ravn <anders@adriansen.dk>
2023-02-13 12:26:14 +0100

    Add: Applied auth router to main router

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,4 +1,10 @@
 const express = require("express");
 const router = express.Router();
+const check = require("../middleware/check");
+
+
+
+// Auth routes
+router.use("/auth", require("./auth"));
 
 module.exports = router;
