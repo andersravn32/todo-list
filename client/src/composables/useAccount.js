3b93518acff9afffb14f9d4a7ab060e2d1d15220
Anders Ravn <anders@adriansen.dk>
2023-02-14 11:31:27 +0100

    Add: Added composable responsible for account func

---
+++ client/src/composables/useAccount.js
@@ -0,0 +1,139 @@
+import { ref } from "vue";
+
+export default () => {
+  const user = ref(null);
+  const refreshToken = ref(null);
+  const accessToken = ref(null);
+
+  const signin = async (email, password) => {
+    const response = await fetch("http://127.0.0.1:3000/auth/signin", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        email,
+        password,
+      }),
+    }).then((res) => res.json());
+
+    // Guard statement
+    if (response.error) {
+      return response;
+    }
+
+    // Update local data store
+    user.value = response.user;
+    refreshToken.value = response.refreshToken;
+    accessToken.value = response.accessToken;
+
+    // Set localStorage
+    localStorage.setItem("user", JSON.stringify(response.user));
+    localStorage.setItem("refreshToken", response.refreshToken);
+    localStorage.setItem("accessToken", response.accessToken);
+
+    // Return initial response object
+    return response;
+  };
+
+  const signup = async (firstName, lastName, email, password) => {
+    const response = await fetch("http://127.0.0.1:3000/auth/signup", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        firstName,
+        lastName,
+        email,
+        password,
+      }),
+    }).then((res) => res.json());
+
+    // Guard statement
+    if (response.error) {
+      return response;
+    }
+
+    // Update local data store
+    user.value = response.user;
+    refreshToken.value = response.refreshToken;
+    accessToken.value = response.accessToken;
+
+    // Set localStorage
+    localStorage.setItem("user", JSON.stringify(response.user));
+    localStorage.setItem("refreshToken", response.refreshToken);
+    localStorage.setItem("accessToken", response.accessToken);
+
+    // Return initial response object
+    return response;
+  };
+
+  const signout = async () => {
+    if (!refreshToken.value) {
+      return;
+    }
+
+    const response = await fetch("http://127.0.0.1:3000/auth/signout", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        token: refreshToken.value,
+      }),
+    });
+
+    // Guard statement
+    if (response.error) {
+      return response;
+    }
+
+    // Reset values
+    user.value = null;
+    refreshToken.value = null;
+    accessToken.value = null;
+
+    // Clear localStorage
+    localStorage.clear();
+
+    // Return response
+    return response;
+  };
+
+  const refresh = async () => {
+    if (!refreshToken.value) {
+      return;
+    }
+
+    const response = await fetch("http://127.0.0.1:3000/auth/refresh", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        token: refreshToken.value,
+      }),
+    });
+
+    // Guard statement
+    if (response.error) {
+      return response;
+    }
+
+    // Update local data store
+    user.value = response.user;
+    refreshToken.value = response.refreshToken;
+    accessToken.value = response.accessToken;
+
+    // Set localStorage
+    localStorage.setItem("user", JSON.stringify(response.user));
+    localStorage.setItem("refreshToken", response.refreshToken);
+    localStorage.setItem("accessToken", response.accessToken);
+
+    // Return initial response object
+    return response;
+  };
+
+  return { user, refreshToken, accessToken, signin, signout, signup, refresh };
+};
