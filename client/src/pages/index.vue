06c3d1150c330d431f760811b3032ebf81a01849
Anders Ravn <anders@adriansen.dk>
2023-02-14 10:02:17 +0100

    Add: Added auth determination logic

--- client/src/pages/index.vue
+++ client/src/pages/index.vue
@@ -1,5 +1,35 @@
+<script setup>
+import Todolist from "../components/Todolist.vue";
+
+const refreshToken = localStorage.getItem("accessToken");
+</script>
 <template>
-    <div>
-        <h1>Hello</h1>
+  <section id="page-todo">
+    <div
+      v-if="!refreshToken"
+      class="border-2 border-slate-300 p-4 rounded w-full max-w-md flex flex-col space-y-4 justify-center items-center text-center"
+    >
+      <h1 class="text-2xl">Log på</h1>
+      <p>
+        Log venligst på, eller opret en ny konto, før du kan benytte denne todo
+        liste
+      </p>
+      <p class="flex items-center space-x-4">
+        <router-link class="text-blue-500 underline" to="/signin"
+          >Log på</router-link
+        >
+        <span>|</span>
+        <router-link class="text-blue-500 underline" to="/signup"
+          >Opret mig</router-link
+        >
+      </p>
     </div>
-    </template>
+    <Todolist v-if="refreshToken" />
+  </section>
+</template>
+
+<style>
+#page-todo {
+  @apply min-h-screen flex flex-col items-center justify-center;
+}
+</style>
