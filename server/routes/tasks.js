d9689b21ee889625af7c4488001a3afd0c2d9258
Anders Ravn <anders@adriansen.dk>
2023-02-14 14:44:25 +0100

    Add: Added put route to tasks router

--- server/routes/tasks.js
+++ server/routes/tasks.js
@@ -10,4 +10,7 @@ router.post("/", require("../controllers/tasks/createTask"));
 // DELETE: /tasks/:id - Deletes specific task by id, if task is owned by requesting user
 router.delete("/:id", require("../controllers/tasks/deleteTask"));
 
+// PUT: /tasks/:id - Updates specific task related to requesting user
+router.put("/:id", require("../controllers/tasks/updateTask"));
+
 module.exports = router;
