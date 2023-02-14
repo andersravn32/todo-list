b8d28fda1a3d8cd2320902e650b1dd37d17e8bfb
Anders Ravn <anders@adriansen.dk>
2023-02-14 10:34:44 +0100

    Add: Added create task capability

---
+++ server/controllers/tasks/createTask.js
@@ -0,0 +1,44 @@
+const database = require("../../utilities/database");
+
+const TODO = {
+  title: null,
+  due: null,
+  done: false,
+};
+
+module.exports = async (req, res) => {
+  // Create new todo object based on static object
+  const todo = {
+    ...TODO,
+    title: req.body.title,
+    due: req.body.due,
+    owner: req.user.uuid,
+  };
+
+  try {
+    // Get active database connection
+    const db = database.get();
+
+    // Insert task data to database
+    const taskInsert = await db.collection("tasks").insertOne(todo);
+
+    // Return error if error is present
+    if (!(taskInsert.acknowledged && taskInsert.insertedId)) {
+      return res.json({
+        error: "Failed to create task",
+      });
+    }
+
+    // Return newly created task object to user
+    return res.json({
+      ...todo,
+      _id: taskInsert.insertedId,
+    });
+    
+  } catch (error) {
+    console.log(error);
+    return res.json({
+      error: error,
+    });
+  }
+};
