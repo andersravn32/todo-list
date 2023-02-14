4799d4a7b69731af831692fcb2d976f90549c35e
Anders Ravn <anders@adriansen.dk>
2023-02-14 10:01:54 +0100

    Add: Improved comments

--- client/src/main.js
+++ client/src/main.js
@@ -3,7 +3,11 @@ import App from "./App.vue";
 import router from "./router";
 import "./style.css";
 
+// Create app instance 
 const app = createApp(App);
+
+// Apply vue router
 app.use(router);
 
+// Mount app to #app div
 app.mount("#app");
