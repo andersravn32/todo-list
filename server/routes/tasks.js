3a996c100719328a08ff1c833f0b6901bec91b0f
Anders Ravn <anders@adriansen.dk>
2023-02-14 10:41:48 +0100

    Add: Added task management

--- server/routes/tasks.js
+++ server/routes/tasks.js
@@ -1,6 +1,13 @@
 const express = require("express");
 const router = express.Router();
 
+// GET: /tasks - Returns all tasks related to requesting user
+router.get("/", require("../controllers/tasks/index"));
 
+// POST: /tasks - Creates new task related to requesting user
+router.post("/", require("../controllers/tasks/createTask"));
+
+// DELETE: /tasks/:id - Deletes specific task by id, if task is owned by requesting user
+router.delete("/:id", require("../controllers/tasks/deleteTask"));
 
 module.exports = router;
