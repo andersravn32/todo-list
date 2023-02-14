78416fe035ec9b85d79f9aff738fc7466de79757
Anders Ravn <anders@adriansen.dk>
2023-02-14 13:42:53 +0100

    Add: Scaffolded todolist

--- client/src/components/Todolist.vue
+++ client/src/components/Todolist.vue
@@ -1,3 +1,23 @@
+<script setup>
+import useAccount from "../composables/useAccount";
+import TodolistHeader from "./TodolistHeader.vue";
+import TodolistContent from "./TodolistContent.vue";
+import TodolistFooter from "./TodolistFooter.vue";
+
+// Import account data from composable
+const account = useAccount();
+</script>
+
 <template>
-    <div id="todolist"></div>
+  <div id="todolist" v-if="account.user.value">
+    <TodolistHeader />
+    <TodolistContent />
+    <TodolistFooter />
+  </div>
 </template>
+
+<style>
+#todolist {
+  @apply border-2 border-slate-300 p-4 w-full max-w-2xl rounded flex flex-col;
+}
+</style>
