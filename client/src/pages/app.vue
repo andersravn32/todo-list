4d49e538142822318b974121e41f21856a6c27df
Anders Ravn <anders@adriansen.dk>
2023-02-14 13:42:24 +0100

    Add: Added todolist to app page

--- client/src/pages/app.vue
+++ client/src/pages/app.vue
@@ -1,6 +1,11 @@
+
+import Todolist from '../components/Todolist.vue';
+<script setup>
+import Todolist from '../components/Todolist.vue';
+</script>
+
 <template>
   <section id="page-app">
-
-    
+    <Todolist />
   </section>
 </template>
