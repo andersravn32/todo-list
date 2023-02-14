a16bfb9300bdbae52f5d639922364ad417544c83
Anders Ravn <anders@adriansen.dk>
2023-02-14 13:12:54 +0100

    Add: Added initial user refresh

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,4 +1,11 @@
 import { createRouter, createWebHistory } from "vue-router";
+import useAccount from "../composables/useAccount";
+
+// Refresh account before initial page load
+const account = useAccount()
+if (account.refreshToken.value) {
+  await account.refresh();
+}
 
 const router = createRouter({
   history: createWebHistory(),
@@ -6,16 +13,40 @@ const router = createRouter({
     {
       path: "/",
       component: () => import("../pages/index.vue"),
+      meta: {
+        auth: false
+      }
     },
     {
       path: "/signin",
       component: () => import("../pages/signin.vue"),
+      meta: {
+        auth: false
+      }
     },
     {
       path: "/signup",
       component: () => import("../pages/signup.vue"),
+      meta: {
+        auth: false
+      }
     },
+    {
+      path: "/app",
+      component: () => import("../pages/app.vue"),
+      meta: {
+        auth: true
+      }
+    }
   ],
 });
+
+router.beforeEach((to) => {
+  if (to.meta.auth && !account.user.value){
+    return router.push("/signin")
+  }
+
+  return true;
+})
 
 export default router;
